54da0d27e7fb6cbd11159e9d6d309a7fbf0c67a1
Priya041199 <priyagawde1712@gmail.com>
2024-12-19 10:51:32 +0530

    HEAD

--- files/fetch_suppliers.py
+++ files/fetch_suppliers.py
@@ -16,8 +16,8 @@ def fetch_suppliers():
         # Display supplier data
         print("Supplier Data:")
         for supplier in suppliers:
-            # print("ID: {supplier['SupplierID']}, Name: {supplier['CompanyName']}, Country: {supplier['Country']}")
-            print("ID: {supplier[1]}")
+            print("ID: {supplier['SupplierID']}, Name: {supplier['CompanyName']}, Country: {supplier['Country']}")
+            # print("ID: {supplier[1]}")
 
     except Exception as e:
         print(f"Error fetching supplier data: {e}")
